92a8b6d5d3f2026c1369db345ebea8a5d83414aa
ItaloProject <ItaloProject@users.noreply.github.com>
2026-06-08 11:22:58 +0000

    deploy: d8463f8d7d10478e5cf85da21d60784aa25e5d42

--- assets/pinia-Do3auYmJ.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-GZ6R3kum.js";var t=e(({app:e,store:t})=>{e.use(t)});export{t as default};
---
+++ assets/theme-K9p5WwE5.js
@@ -0,0 +1,1 @@
+import{z as e}from"./index-DOvJPmzx.js";import{t}from"./theme-DdCM7fhE.js";var n=e(()=>{t().init()});export{n as default};
